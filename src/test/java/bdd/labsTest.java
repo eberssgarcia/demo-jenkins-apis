403512bd81c1014388b08e1eb18b6f521eada8f2
Eber García <ebgarcia@baufest.com>
2025-04-22 16:11:18 -0500

    Actualizando jenkinsfile

--- src/test/java/bdd/labsTest.java
+++ src/test/java/bdd/labsTest.java
@@ -3,23 +3,35 @@ package bdd;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static util.cucumberReport.generateReport;
 
 public class labsTest {
 
+    private static final Logger logger = Logger.getLogger(labsTest.class.getName());
+
+    @BeforeAll
+    static void beforeAll() {
+        logger.info("BEFORE ALL");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        logger.info("AFTER ALL");
+    }
 
     @Test
     void testParallel() {
         Results results = Runner.path("classpath:bdd")
-                .tags("@token")
-                .outputCucumberJson(true)
-                .reportDir("target/cucumber-reports")  // ← agrega esto
-                .parallel(5);
-
+//                .tags("@token")
+                .outputCucumberJson(true).parallel(5);
         generateReport(results.getReportDir());
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
